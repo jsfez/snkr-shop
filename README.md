0033feae833c28425e7ae68e4c4263bb3db09928
Jeremy Sfez <jeremy@smooth-code.com>
2023-12-04 13:50:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SNKR shop
+
+SNKR shop is a copy of (snkr-hub)[ttps://github.com/yourusername/snkr-hub.git] project.
+
+## Run the Development Server
+
+```bash
+npm run dev
+```
---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CartReducer, { setCartFromLocalStorage } from "./slices/CartSlice";
+
+export const store = configureStore({
+  reducer: {
+    cart: CartReducer,
+  },
+});
+
+// Use subscribe to listen for changes in the store
+store.subscribe(() => {
+  const state = store.getState();
+  console.log(state);
+  localStorage.setItem("localCart", JSON.stringify(state.cart));
+});
+
+const loadCartFromLocalStorage = () => {
+  const storedCart = localStorage.getItem("localCart");
+  if (storedCart) {
+    const parsedCart = JSON.parse(storedCart);
+
+    store.dispatch(setCartFromLocalStorage(parsedCart));
+  }
+};
+loadCartFromLocalStorage();
