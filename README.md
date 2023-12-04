327213edbf70f7a6e94a51c81174d0e48554e878
Jeremy Sfez <jeremy@smooth-code.com>
2023-12-04 14:43:04 +0100

    feat: update playwright devices

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SNKR shop
 
-SNKR shop is a copy of (snkr-hub)[ttps://github.com/yourusername/snkr-hub.git] project.
+SNKR shop is a copy of [snkr-hub](ttps://github.com/yourusername/snkr-hub.git) project.
 
 ## Run the Development Server
 
--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
+      name: "chrome",
       use: { ...devices["Desktop Chrome"] },
     },
 
@@ -43,20 +43,20 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: "iPhone-12",
+      use: { ...devices["iPhone 12"] },
+    },
 
     /* Test against branded browsers. */
     // {
