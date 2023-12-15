2a0cb1be0328ac7c8201b85a905a8fd3d831a387
Jeremy Sfez <jeremy@smooth-code.com>
2023-12-15 14:57:05 +0100

    feat: remove some data

--- tests/screenshot-pages.spec.ts
+++ tests/screenshot-pages.spec.ts
@@ -6,13 +6,12 @@ const pages = [
   { name: "homepage", path: "/" },
   { name: "list", path: "/explore" },
   { name: "product-detail", path: "/preview/404758" },
-  { name: "cart", path: "/cart" },
 ];
 
 test("Screenshot pages", async ({ page }, workerInfo) => {
   for (const { name, path } of pages) {
     const browserName = workerInfo.project.name;
     await page.goto(`${baseUrl}${path}`);
-    await argosScreenshot(page, `${name}-${browserName}`);
+    await argosScreenshot(page, `${name}-${browserName}`, { fullPage: false });
   }
 });
